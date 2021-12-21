6f17e18c9953d53b0a97d49c2cd0e535f98668ca
MuhsinYgc <96091541+MuhsinYgc@users.noreply.github.com>
2021-12-22 00:33:36 +0300

    My movies project-part-2

---
+++ App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import MovieList from "./MovieList";
+import SearchBar from "./SearchBar";
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    movies: [],
+    searchQuery: "",
+  };
+
+  // async componentDidMount() {
+  //   const baseURL = "http://localhost:3002/movies";
+  //   const response = await fetch(baseURL);
+  //   console.log(response);
+  //   const data = await response.json();
+  //   console.log(data);
+  //   this.setState({ movies: data });
+  // }
+
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:3002/movies");
+    console.log(response);
+    this.setState({ movies: response.data });
+  }
+
+  // deleteMovie = (movie) => {
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+  //   // this.setState({
+  //   //   movies: newMovieList,
+  //   // });
+  //   this.setState((state) => ({
+  //     movies: newMovieList,
+  //   }));
+  // };
+  //fetch api
+  // deleteMovie = async (movie) => {
+  //   const baseURL = `http://localhost:3002/movies/${movie.id}`;
+  //   await fetch(baseURL, {
+  //     method: "Delete",
+  //   });
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+  //   this.setState((state) => ({ movies: newMovieList }));
+  // };
+
+  deleteMovie = async (movie) => {
+    axios.delete(`http://localhost:3002/movies/${movie.id}`);
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+    this.setState((state) => ({ movies: newMovieList }));
+  };
+
+  searchMovie = (event) => {
+    console.log(event.target.value);
+    this.setState({ searchQuery: event.target.value });
+  };
+
+  render() {
+    let filtredMovies = this.state.movies.filter((movie) => {
+      return (
+        movie.name
+          .toLowerCase()
+          .indexOf(this.state.searchQuery.toLowerCase()) !== -1
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <SearchBar searchMovieProp={this.searchMovie} />
+          </div>
+        </div>
+
+        <MovieList movies={filtredMovies} deleteMovieProp={this.deleteMovie} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ SearchBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <div className="form-row mb-5">
+          <div className="col-12">
+            <input
+              onChange={this.props.searchMovieProp}
+              type="text"
+              className="form-control"
+              placeholder="Search a movie"
+            ></input>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
